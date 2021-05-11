4f9c7cd8cec4fa61462d6078f1aebc8ace9aa729
Sirio Bolaños Puchet <sirio.bolanospuchet@epfl.ch>
2021-05-11 17:52:33 +0200

    Adapt and integrate lightweight trees, > 7x speedup

--- code/lwtree.py
+++ code/lwtree.py
@@ -1,98 +1,99 @@
+# lwtree: lightweight trees
+# AUTHOR: Michael Reimann <michael.reimann@epfl.ch>
+# AUTHOR: Sirio Bolaños Puchet <sirio.bolanospuchet@epfl.ch>
+# LAST MODIFIED: 2021-05-11
+
 import numpy
-import pandas
-import tqdm
 import brotli
-import collections
 import tarfile
-from io import StringIO
+import collections
+
+# tqdm is optional dependency
+try:
+    import tqdm
+    iter_status_fun = lambda t, x, **kw: tqdm.tqdm(x, **kw) if t else x
+except ImportError:
+    iter_status_fun = lambda t, x, **kw: x
 
 
-def maximal_simplices(T):
-    if len(T) == 0:
-        return [[]]
-    ret = []
-    for gid, C in T.items():
-        ret.extend([[gid] + smpl for smpl in maximal_simplices(C)])
-    return ret
+def simplices(T, maximal_only=False):
+    """
+    Returns a list of tuples, where:
 
+    [0]: list of GIDs in simplex
+    [1]: maximality indicator
 
-def simplices(T):
+    If maximal_only, only returns maximal simplices.
+    """
     def recursive(T):
         ret = [[]]
-        for gid, C in T.items():
-            ret.extend([[gid] + smpl for smpl in recursive(C)])
+        for (gid, m), C in T.items():
+            ret.extend([[(gid, m)] + smpl for smpl in recursive(C)])
         return ret
     ret = []
-    for gid, C in T.items():
-        ret.extend([[gid] + smpl for smpl in recursive(C)])
-    return ret
+    for (gid, m), C in T.items():
+        ret.extend([[(gid, m)] + smpl for smpl in recursive(C)])
+    ret = [([x[0] for x in simp], numpy.sum([x[1] for x in simp])) for simp in ret]
+    return ret if not maximal_only else [x for x in ret if x[1]]
 
 
-def maximal_simplex_counts(T):
-    if len(T) == 0:
-        return {-1: 1}
-    ret = {}
-    for C in T.values():
-        for dim, count in maximal_simplex_counts(C).items():
-            ret[dim + 1] = ret.get(dim + 1, 0) + count
-    return ret
-
-
-def simplex_counts(T):
-    def recursive(T):
-        ret = {-1: 1}
-        for C in T.values():
-            for dim, count in recursive(C).items():
+def simplex_counts(T, maximal_only=False):
+    """
+    Count simplices by accumulating while recursively walking a simplex tree
+    """
+    def recursive(T, is_max):
+        if maximal_only and is_max:
+            return {-1: 1}
+        ret = {} if maximal_only else {-1: 1}
+        for (_, is_max), C in T.items():
+            for dim, count in recursive(C, is_max).items():
                 ret[dim + 1] = ret.get(dim + 1, 0) + count
         return ret
-    ret = {}
-    for C in T.values():
-        for dim, count in recursive(C).items():
-            ret[dim + 1] = ret.get(dim + 1, 0) + count
+    ret = recursive(T, False)
+    ret.pop(-1, None) 
     return ret
 
 
+# default factory for defaultdict
 def LwTree():
     return collections.defaultdict(LwTree)
 
 
-def build_tree_lw(lvls, gids):
+def build_tree_lw(lvls, gids, maxs):
+    """
+    Build tree as a dictionary of dictionaries,
+    where keys are tuples (GID, is_maximal)
+    """
     T = LwTree()
-    parents = {0: T}
-    for lvl, gid in zip(lvls, gids):
+    parents = {0: T}  # temporary storage of last seen parent at some level
+    for lvl, gid, m in zip(lvls, gids, maxs):
         t = parents[lvl]
-        _ = t[gid]
-        parents[lvl + 1] = t[gid]
+        parents[lvl + 1] = t[(gid, m)]
     return T
 
 
-def parse_lwtree_pd(bytes_struc, maxdim=10):
-    raw = bytes_struc.decode("utf-8")
-    data = pandas.read_csv(StringIO(raw.replace("*", "")),
-                           sep=' ',
-                           header=None,
-                           names=[str(_x) for _x in range(maxdim)]).values
-    x, lvls = numpy.nonzero(~numpy.isnan(data))
-    gids = data[x, lvls].astype(int)
-    return build_tree_lw(lvls, gids)
-
-
 def parse_lwtree_np(bytes_struc):
-    raw = bytes_struc.decode("utf-8").replace("*", "")
+    """
+    Parse text representation of a tree as a list of UTF-8 lines,
+    extracting level, gid and maximal indicator from each line
+    """
+    raw = bytes_struc.decode("utf-8")
     raw_splt = raw.split("\n")
     lvls = [row.count(" ") for row in raw_splt]
-    gids = [int(row) for row in raw_splt if len(row) > 0]
-    return build_tree_lw(lvls, gids)
+    maxs = [row.count("*") for row in raw_splt]  # maximal simplex indicator
+    gids = [int(row[:-1]) if maxs[i] else int(row) for i, row in enumerate(raw_splt) if len(row) > 0]
+    return build_tree_lw(lvls, gids, maxs)
 
 
-def load_lwtrees(file_tar, maxproc=None):
+def load_lwtrees(file_tar, maxproc=None, show_progress=True):
     """
     Load simplex trees from a TAR archive holding brotli-compressed text trees.
     """
     lwtrees = []
     with tarfile.open(file_tar, 'r:') as tf:
         membs = tf.getmembers()
-        for i, memb in tqdm.tqdm(enumerate(membs), total=numpy.minimum(maxproc, len(membs))):
+        for i, memb in iter_status_fun(show_progress, enumerate(membs),
+                                       total=numpy.minimum(maxproc, len(membs))):
             if maxproc is not None and i >= maxproc:
                 break
             with tf.extractfile(memb) as f:
--- code/simptree.py
+++ code/simptree.py
@@ -1,24 +1,16 @@
 # py-simptree: process simplex trees in python
 # AUTHOR: Sirio Bolaños Puchet <sirio.bolanospuchet@epfl.ch>
-# LAST MODIFIED: 2021-04-27
+# LAST MODIFIED: 2021-05-11
 
-import treelib
-import tarfile
-import brotli
+import lwtree
 
 
 def count_simplices(root):
     """
     Count simplices per dimension.
     """
-    maxdim = root.depth()
-    counts = [0] * (maxdim + 1)
-    counts_max = [0] * (maxdim + 1)
-    for nid in root.expand_tree(mode=treelib.Tree.DEPTH):
-        level = root.depth(nid)
-        counts[level] += 1
-        if root[nid].data:  # is maximal
-            counts_max[level] += 1
+    counts = lwtree.simplex_counts(root, False)
+    counts_max = lwtree.simplex_counts(root, True)
     return counts, counts_max
 
 
@@ -26,89 +18,17 @@ def print_simplices(root, maximal_only=False):
     """
     Print simplex members, source to sink, mark with '*' if maximal.
     """
-    gids = []
-    for nid in root.expand_tree(mode=treelib.Tree.DEPTH):
-        node = root[nid]
-        if maximal_only and not node.data:  # skip if maximal only and not maximal
-            continue
-        gids.append(str(node.tag))
-        parent = root.parent(nid)
-        while parent is not None:  # backwards path traversal
-            gids.append(str(parent.tag))
-            nid = parent.identifier
-            parent = root.parent(nid)
-        print(' '.join(gids + ['*' if node.data and not maximal_only else '']))
-        gids = []  # clear
+    simp = lwtree.simplices(root, maximal_only)
+    for (s, m) in simp:
+        [print(x, end=' ') for x in s[::-1]]
+        print('*') if m and not maximal_only else print('')
 
 
-# FIXME: faster python implementation possible?
-def parse_simptree(bstr, maxdim=None):
-    """
-    Parse a treelib.Tree from a bytestring. Adapted from C implementation.
-    """
-    GIDMAX_DIGITS = 10  # maximum number of digits in a uint32
-    DIGITS = [ord('0'), ord('1'), ord('2'), ord('3'), ord('4'), ord('5'), ord('6'), ord('7'), ord('8'), ord('9')]
-
-    # state vars
-    numbuf = []
-    nump = 0
-    level = 1
-    is_maximal = False
-
-    root = treelib.Tree()  # tree object
-
-    # last seen parent per level
-    last = {}
-    last[0] = None
-
-    for c in bstr:
-        if c in DIGITS:
-            if nump > GIDMAX_DIGITS - 1:
-                raise Exception("Number too long: length > {}".format(GIDMAX_DIGITS))
-            numbuf.append(chr(c))
-        elif c == ord(' '):
-            level += 1
-        elif c == ord('*'):
-            is_maximal = True
-        elif c == ord('\n'):
-            gid = int(''.join(numbuf))  # char list to string to integer
-            if maxdim is None or level <= maxdim + 1:
-                last[level] = root.create_node(gid, parent=last[level - 1], data=is_maximal)
-            # reset state vars
-            numbuf = []
-            nump = 0
-            level = 1
-            is_maximal = False
-        else:
-            raise Exception("Invalid character found: '{}'".format(chr(c)))
-
-    return root
-
-
-def load_simptrees(file_tar, maxdim=None, maxproc=None, verbose=False):
+def load_simptrees(file_tar, maxproc=None, show_progress=True):
     """
     Load simplex trees from a TAR archive holding brotli-compressed text trees.
     """
-    if verbose:
-        import sys
-
-    simptrees = []
-    with tarfile.open(file_tar, 'r:') as tf:
-        for i, memb in enumerate(tf.getmembers()):
-            if maxproc is not None and i >= maxproc:
-                break
-            if verbose:
-                print("Now processing file {}".format(memb.name), file=sys.stderr)
-            with tf.extractfile(memb) as f:
-                compr_data = f.read1(memb.size)
-                assert(len(compr_data) == memb.size)
-                data = brotli.decompress(compr_data)
-                del compr_data  # free early
-                tree = parse_simptree(data, maxdim)
-                del data  # free early
-                simptrees.append(tree)
-
-    return simptrees
+    return lwtree.load_lwtrees(file_tar, maxproc, show_progress)
 
 
 # convenience CLI
@@ -118,10 +38,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("py-simptree")
     parser.add_argument("file_tar", nargs='+', help="TAR archive holding brotli-compressed text trees")
     parser.add_argument("-c","--count", default=True, action="store_true", help="Count simplices per dimension")
-    parser.add_argument("-d","--maxdim", type=int, default=None, help="Maximum simplex dimension to process")
     parser.add_argument("-m","--maximal-only", default=False, action="store_true", help="Print maximal simplices only")
     parser.add_argument("-p","--print", default=False, action="store_true", help="Print simplices")
-    parser.add_argument("-v","--verbose", default=False, action="store_true", help="Verbose messages")
+    parser.add_argument("--no-progress", default=False, action="store_true", help="Do not show progress")
     parser.add_argument("--maxproc", type=int, default=None, help="Process up to this number of files in total")
 
     import sys
@@ -133,9 +52,9 @@ if __name__ == "__main__":
 
     tree_counts = []
     tree_counts_max = []
-    maxproc = args.maxproc  # max files to process
+    maxproc = args.maxproc  # max trees to process
     for fname in args.file_tar:
-        simptrees = load_simptrees(fname, args.maxdim, maxproc, args.verbose)
+        simptrees = load_simptrees(fname, maxproc, not args.no_progress)
         if maxproc is not None:
             maxproc -= len(simptrees)
 
@@ -154,8 +73,9 @@ if __name__ == "__main__":
     if args.print:
         pass
     elif args.count:
-        import itertools
-        counts = [sum(i) for i in itertools.zip_longest(*tree_counts, fillvalue=0)]
-        counts_max = [sum(i) for i in itertools.zip_longest(*tree_counts_max, fillvalue=0)]
-        for i, (c, cm) in enumerate(itertools.zip_longest(counts, counts_max, fillvalue=0)):
-            print('{} {} {}'.format(i, c, cm))
+        from functools import reduce
+        from collections import Counter
+        counts = reduce(lambda x, y: Counter(x) + Counter(y), tree_counts)
+        counts_max = reduce(lambda x, y: Counter(x) + Counter(y), tree_counts_max)
+        for k in counts.keys():
+            print('{} {} {}'.format(k, counts[k], counts_max.get(k, 0)))
